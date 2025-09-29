8fdd50fc125779ccc9a3ce15d14514f408fbdfda
ZwergenElf <schwedron@email>
2025-09-30 00:27:36 +0200

    first solo bingo features

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,34 @@
 import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+
+enum COLOR {
+  RED = 'red',
+  BLUE = 'blue',
+  GREEN = 'green',
+  YELLOW = 'yellow',
+  ORANGE = 'orange',
+  PURPLE = 'purple',
+}
+
+type Field = {
+  task: string;
+  obtainedBy: COLOR | null;
+};
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
   templateUrl: './app.html',
-  styleUrl: './app.scss'
+  styleUrl: './app.scss',
 })
 export class App {
-  protected readonly title = signal('bingo');
+  protected readonly fields = signal<Array<Field>>(Array(25).fill({ task: '', obtainedBy: null }));
+  protected readonly COLOR = COLOR;
+
+  mark(index: number, color: COLOR) {
+    this.fields.update((fields) => {
+      const field = fields[index];
+      const newColor = field.obtainedBy === color ? null : color;
+      fields[index] = { ...field, obtainedBy: newColor };
+      return fields;
+    });
+  }
 }
